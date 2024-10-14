bbb53743af346e4e2cca6090734e78532700e0e1
croblesr <croblesr@ripley.com>
2024-10-14 17:04:00 -0300

    [crobles] - [feature :sparkles:]: [x] - [adding functions to transform json to object to graph]
    -
    -

--- node-tree-backend/src/cascade/application/node-cascade-processing-data.use-case.ts
+++ node-tree-backend/src/cascade/application/node-cascade-processing-data.use-case.ts
@@ -1,9 +1,64 @@
 import { Injectable } from '@nestjs/common';
 import { UseCase } from '../../shared/interfaces/use-case.interface';
+import { nodeTree } from '../../shared/json-files/nodeTree';
 
 @Injectable()
 export class NodeCascadeProcessingDataUseCase implements UseCase {
+  private config: any;
+  constructor() {
+    this.loadConfig();
+  }
+  private loadConfig() {
+    console.log(typeof nodeTree);
+    this.config = nodeTree;
+  }
   async run(): Promise<any> {
-    return true;
+    const dataNode = this.transformDataNode(this.config);
+    const dataTree = this.transformDataTree(dataNode);
+    return JSON.stringify(dataTree, null, 2);
+  }
+
+  transformDataNode(data: any): any {
+    const idMap = data.reduce((acc, curr) => {
+      acc[curr.id] = curr;
+      return acc;
+    }, {});
+
+    return data.map((item) => {
+      if (item.nodeList) {
+        item.nodeList = item.nodeList.map((id) => {
+          const nodeObject = idMap[id];
+          return {
+            id: nodeObject.id,
+            node: nodeObject.node,
+            value: item.value,
+          };
+        });
+      }
+      return item;
+    });
+  }
+
+  transformDataTree(data: any): any {
+    const graph = {};
+
+    data.forEach((item) => {
+      graph[item.id] = {
+        id: item.id,
+        node: item.node,
+        value: item.value,
+        children: [],
+      };
+    });
+
+    data.forEach((item) => {
+      if (item.nodeList) {
+        item.nodeList.forEach((child) => {
+          graph[item.id].children.push(graph[child.id]);
+        });
+      }
+    });
+
+    return graph[1];
   }
 }
