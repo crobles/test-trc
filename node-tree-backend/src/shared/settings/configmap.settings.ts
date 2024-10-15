1f55709a141a4c26528e6ce43ce5e92c852f9357
croblesr <croblesr@ripley.com>
2024-10-15 04:18:56 -0300

    [croblesr] - [x]: [x] - [x]
    -
    -

--- node-tree-backend/src/shared/settings/configmap.settings.ts
+++ node-tree-backend/src/shared/settings/configmap.settings.ts
@@ -1,5 +1,5 @@
 export const configmapSettings = {
-  APP_PORT: Number(process.env.APP_PORT) || 3000,
+  APP_PORT: Number(process.env.APP_PORT) || 3100,
   APP_ID: process.env.APP_ID,
   NODE_ENV: process.env.NODE_ENV,
 };
